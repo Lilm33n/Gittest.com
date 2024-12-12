56554539f9f81a92faddc277d3d4f5f72caa9ee3
Alamin <danladialamin42@gmail.com>
2024-12-13 00:16:24 +0200

    my first commit

---
+++ gittest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int name;
+    cout << "Hello World!\n";
+    cout << "this is a test\n";
+    cout << "this is a new\n";
+    cout << "this is a black\n";
+    cin >> name;
+}
+
+
